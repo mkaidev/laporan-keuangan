b77bb9c3f71d5ffc02582198c4a332effc4f5736
novipro88 <noviantipali97@gmail.com>
2024-05-27 21:38:01 +0700

    Setup authentication

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Loader2 } from "Lucide-react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="flex items-center justify-center pt-16 lg:pt-4">
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image src="/finance.svg" height={500} width={500} alt="finance" />
+      </div>
+    </div>
+  );
+}
